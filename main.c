10d81520ae10a690ec0312ece4385b3ca9ff9f6c
御厨 洋 <mickly9@MacBookAirHiroshi.local>
2019-03-11 11:12:33 +0900

    mod

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include "mpu9250.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
+    int res = init_mpu9250(0x68);
+    if (res != 0)
+        return 1;
+    for (;;) {
+        short gyro[3] = { 0 };
+        short accel[3] = { 0 };
+        if (read_accel(accel) != 0)
+            return 1;
+        if (read_gyro(gyro) != 0)
+            return 1;
+        printf("%8d%8d%8d%8d%8d%8d\n", accel[0], accel[1], accel[2], gyro[0], gyro[1], gyro[2]);
+        sleep(1);
+    }
     return 0;
 }
